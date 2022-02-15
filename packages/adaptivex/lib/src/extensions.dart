42247397f2c1ff9dae339a0cdc0b493198c2483a
kechankrisna <ke.chankrisna168@gmail.com>
2022-02-15 14:42:06 +0700

    referece from w3school

--- packages/adaptivex/lib/src/extensions.dart
+++ packages/adaptivex/lib/src/extensions.dart
@@ -212,20 +212,3 @@ enum ScreenSize { xs, sm, md, lg, xl }
 
 /// refer to the box width
 enum BoxSize { xs, sm, md, lg, xl }
-
-/// Reference from w3school
-
-/* Extra small devices (phones, 600px and down) */
-/// @media only screen and (max-width: 600px) {...}
-
-/* Small devices (portrait tablets and large phones, 600px and up) */
-/// @media only screen and (min-width: 600px) {...}
-
-/* Medium devices (landscape tablets, 768px and up) */
-/// @media only screen and (min-width: 768px) {...}
-
-/* Large devices (laptops/desktops, 992px and up) */
-/// @media only screen and (min-width: 992px) {...}
-
-/* Extra large devices (large laptops and desktops, 1200px and up) */
-/// @media only screen and (min-width: 1200px) {...}
