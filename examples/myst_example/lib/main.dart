6816523c1f898293ef4dc87c408c51ade4749346
kechankrisna <ke.chankrisna168@gmail.com>
2022-03-02 22:43:48 +0700

    blank example

--- examples/myst_example/lib/main.dart
+++ examples/myst_example/lib/main.dart
@@ -3,10 +3,9 @@ import 'package:myst_example/core.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  await ApplicationService.ensureSharedPreferences();
+  /// await ApplicationService.ensureSharedPreferences();
 
   var authenticationController = AuthenticationController();
 
   runApp(MyApp(authenticationController: authenticationController));
 }
-
